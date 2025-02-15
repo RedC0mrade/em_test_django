f4aa7ba4f45c6aef2f2c7980d39fe76649b8d767
RedC0mrade <e2e4e2e4@mail.ru>
2025-02-15 22:38:28 +0200

    index urls views

--- em_django/cafe_em/views.py
+++ em_django/cafe_em/views.py
@@ -1,3 +1,25 @@
+from django.http import (
+    HttpRequest,
+    HttpResponse,
+)
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    DetailView,
+)
 
-# Create your views here.
+from .models import Dish, Order, OrderItem
+
+
+def index_view(request: HttpRequest) -> HttpResponse:
+    orders = Order.objects.prefetch_related("order_items__dish").all()
+    return render(
+        request,
+        template_name="cafe_em/index.html",
+        context={"orders": orders},
+    )
+
+
+class ToDoListIndexView(ListView):
+    template_name = "cafe_em/index.html"
+    queryset = Order.objects.all()[:3]
